b3048751be778e43f485b82296e67cc92a117b43
poommin2543 <poommin2543@gmail.com>
2021-08-20 10:55:53 +0700

    update

--- opencv.py
+++ opencv.py
@@ -4,6 +4,15 @@ import moviepy.editor as mp
 cap = cv2.VideoCapture('./image/IMG_34731.mp4')
 fgbg = cv2.createBackgroundSubtractorMOG2()
 
+def find_street_lanes(image):
+    grayscale_image = grayscale(image)
+    blur_image = blur(grayscale_image)
+    canny_image = canny(blur_image)
+    roi_image = roi(canny_image)
+    hough_lines_image = hough_lines(roi_image, 0.9, np.pi/180, 100, 100, 50)
+    final_image = combine_images(hough_lines_image, image)
+
+
 while True:
     ret, frame = cap.read()
     frameb = cv2.resize(frame, (1280, 720), fx=0, fy=0, interpolation=cv2.INTER_CUBIC)
